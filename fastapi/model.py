eb62de7336ed0f49ec1fe6104825c67d436ca03c
ddde8 <105919676+ddde8@users.noreply.github.com>
2025-09-16 01:27:00 +0900

    update to fastapi

--- fastapi/model.py
+++ fastapi/model.py
@@ -8,6 +8,26 @@ class Item(BaseModel):
     item: str
     status: str
 
+'''
 class Todo(BaseModel):
     id: int
     item: Item
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example Schema!"
+            }
+        }
+'''
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
